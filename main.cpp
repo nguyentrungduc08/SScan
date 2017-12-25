752f783cb0fe1afd6a8bbc2c89c631a8188098b4
hydra <nguyentrungduc08@gmail.com>
2017-12-25 10:25:16 +0700

    delete gettime

--- main.cpp
+++ main.cpp
@@ -34,26 +34,7 @@ void init();
 void buildCMDMasscan();
 void getListHostIP();
 
-void time(){
-    time_t rawtime;
-    struct tm * ptm;
 
-    time ( &rawtime );
-
-    ptm = gmtime ( &rawtime );
-
-    puts ("Current time around the World:");
-    printf ("Phoenix, AZ (U.S.) :  %2d:%02d\n", (ptm->tm_hour+MST)%24, ptm->tm_min);
-    printf ("Reykjavik (Iceland) : %2d:%02d\n", (ptm->tm_hour+UTC)%24, ptm->tm_min);
-    printf ("Beijing (China) :     %2d:%02d\n", (ptm->tm_hour+CCT)%24, ptm->tm_min);
-    
-    time_t t = time(0);   // get time now
-    struct tm * now = localtime( & t );
-    cout << (now->tm_year + 1900) << '-' 
-         << (now->tm_mon + 1) << '-'
-         <<  now->tm_mday
-         << endl;
-}
 
 int getdate(){
     time_t t = time(0);   // get time now
